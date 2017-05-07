6ed499ab8bc62f979cd2d1e4a278c038615edc6b
EdaletMemmedli <edalet96@gmail.com>
2017-05-08 02:53:05 +0400

    2017-08-05

---
+++ index.php
@@ -0,0 +1,65 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="reset.css">
+    <link rel="stylesheet" href="text.css">
+    <link rel="stylesheet" href="960.css">
+    <link rel="stylesheet" href="default.css">
+
+</head>
+<body>
+    <div class="container_12" id="main_wrapper">
+        <div class="grid_12" id="header">
+            <div class="grid_4" id="logo">
+                <a href="#">Logo</a>
+            </div>
+            <div class="grid_6" id="search">
+                <form>
+                    <input type="text" name="serachKeyWords" placeholder="Type what do you want">
+                    <input type="submit" name="searchBtn" value="Search">
+                </form>
+            </div>
+        </div>
+        <div class="grid_12" id="nav">
+            <ul>
+                <li><a href="#">Nav</a></li>
+                <li><a href="#">Nav</a></li>
+                <li><a href="#">Nav</a></li>
+                <li><a href="#">Nav</a></li>
+                <li><a href="#">Nav</a></li>
+                <li><a href="#">Nav</a></li>
+            </ul>
+        </div>
+        <div class="grid_8" id="slider"></div>
+        <div class="grid_4" id="side_bar"></div>
+        <div class="grid_12" id="bests">
+            <div class="grid_2" id="best"></div>
+            <div class="grid_2" id="best"></div>
+            <div class="grid_2" id="best"></div>
+            <div class="grid_2" id="best"></div>
+            <div class="grid_2" id="best"></div>
+            <div class="grid_2" id="best"></div>
+        </div>
+        <div class="grid_12" id="articles">
+            <div class="grid_4" id="article"></div>
+            <div class="grid_4" id="article"></div>
+            <div class="grid_4" id="article"></div>
+            <div class="grid_4" id="article"></div>
+            <div class="grid_4" id="article"></div>
+            <div class="grid_4" id="article"></div>
+        </div>
+        <div class="grid_12" id="footer">
+            <div class="grid_5" id="contact"></div>
+            <div class="grid_4" id="support"></div>
+            <div class="grid_3" id="about"></div>
+        </div>
+
+    </div>
+
+</body>
+</html>
